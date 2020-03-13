677a78b7a2e57dfc4b4849fa8eabce8e18295e05
kmoki <futuredictator30@yahoo.co.jp>
2020-03-14 02:07:38 +0900

    stateとイベントハンドリング

--- src/App.js
+++ src/App.js
@@ -3,16 +3,48 @@ import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {
+          title: "デフォルトTODO",
+          id: 0
+        }
+      ],
+      uniqueId: 1
+    };
+    this.addTodo = this.addTodo.bind(this);
+    this.resetTodo = this.resetTodo.bind(this);
+  }
+
+  addTodo(title) {
+    const { tasks, uniqueId } = this.state;
+
+    tasks.push({
+      title,
+      id: uniqueId
+    });
+
+    this.setState({
+      tasks,
+      uniqueId: uniqueId + 1
+    });
+  }
+
+  resetTodo() {
+    this.setState({
+      tasks: []
+    });
+  }
+
   render() {
-    const tasks = [
-      { title: "Todo1つ目", id: 0 },
-      { title: "Todo2つ目", id: 1 }
-    ];
     return (
-      <div>
+      <div className="App">
         <h1>TODO App</h1>
-        <TodoInput />
-        <TodoList tasks={tasks} />
+        <button onClick={this.resetTodo}>リセット</button>
+        <TodoInput addTodo={this.addTodo} />
+        <TodoList tasks={this.state.tasks} />
       </div>
     );
   }
